533e9d1436dd2183ba0830dfe745358d0ffe1d17
Kamalnath S <154576809+Kamalnath1008@users.noreply.github.com>
2024-03-20 19:18:39 +0530

    Update settings.py

--- ecart_project/settings.py
+++ ecart_project/settings.py
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'ecart_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecart',
-        'HOST': 'localhost',
-        'PASSWORD':'',
-        'USER':'root',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'ecart',
+        # 'HOST': 'localhost',
+        # 'PASSWORD':'',
+        # 'USER':'root',
+        # 'PORT':'3306'
     }
 }
 
